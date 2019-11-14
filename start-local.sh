450894e7630fd9ea7af9e52cc265fb93c6b2645a
Chris <christoph.schranz@salzburgresearch.at>
2019-11-14 11:56:43 +0100

    removed invalid network parameter

--- start-local.sh
+++ start-local.sh
@@ -12,7 +12,7 @@ esac; shift; done
 # Check if arguments are valid
 if [[ $PORT != [0-9][0-9][0-9][0-9]* ]]; then
     echo "Given port is not valid."
-    echo "Usage: $0 -p [port] -n [docker-network]  # port must be an integer with 4 or more digits."
+    echo "Usage: $0 -p [port]  # port must be an integer with 4 or more digits."
     exit 21
 fi
 
@@ -23,5 +23,5 @@ export JUPYTER_PORT=$PORT
 # echo $JUPYTER_PORT
 docker-compose up --build -d
 echo
-echo "Started gpu-jupyter via docker-compose on port $JUPYTER_PORT."
+echo "Started gpu-jupyter via docker-compose on localhost:$JUPYTER_PORT."
 echo "See docker-compose logs -f for logs."
