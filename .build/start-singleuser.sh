a4d55736980f3919bdff8fee8a184973a097d60a
Christoph Schranz <christoph.schranz@salzburgresearch.at>
2020-06-22 13:26:01 +0200

    feature: set sparse Dockerfile with Python interpreter only

--- .build/start-singleuser.sh
+++ .build/start-singleuser.sh
@@ -6,7 +6,7 @@ set -e
 
 # set default ip to 0.0.0.0
 if [[ "$NOTEBOOK_ARGS $@" != *"--ip="* ]]; then
-  NOTEBOOK_ARGS="--ip=0.0.0.0 $NOTEBOOK_ARGS"
+    NOTEBOOK_ARGS="--ip=0.0.0.0 $NOTEBOOK_ARGS"
 fi
 
 # handle some deprecated environment variables
@@ -14,30 +14,26 @@ fi
 # These won't be passed from DockerSpawner 0.9,
 # so avoid specifying --arg=empty-string
 if [ ! -z "$NOTEBOOK_DIR" ]; then
-  NOTEBOOK_ARGS="--notebook-dir='$NOTEBOOK_DIR' $NOTEBOOK_ARGS"
+    NOTEBOOK_ARGS="--notebook-dir='$NOTEBOOK_DIR' $NOTEBOOK_ARGS"
 fi
 if [ ! -z "$JPY_PORT" ]; then
-  NOTEBOOK_ARGS="--port=$JPY_PORT $NOTEBOOK_ARGS"
+    NOTEBOOK_ARGS="--port=$JPY_PORT $NOTEBOOK_ARGS"
 fi
 if [ ! -z "$JPY_USER" ]; then
-  NOTEBOOK_ARGS="--user=$JPY_USER $NOTEBOOK_ARGS"
+    NOTEBOOK_ARGS="--user=$JPY_USER $NOTEBOOK_ARGS"
 fi
 if [ ! -z "$JPY_COOKIE_NAME" ]; then
-  NOTEBOOK_ARGS="--cookie-name=$JPY_COOKIE_NAME $NOTEBOOK_ARGS"
+    NOTEBOOK_ARGS="--cookie-name=$JPY_COOKIE_NAME $NOTEBOOK_ARGS"
 fi
 if [ ! -z "$JPY_BASE_URL" ]; then
-  NOTEBOOK_ARGS="--base-url=$JPY_BASE_URL $NOTEBOOK_ARGS"
+    NOTEBOOK_ARGS="--base-url=$JPY_BASE_URL $NOTEBOOK_ARGS"
 fi
 if [ ! -z "$JPY_HUB_PREFIX" ]; then
-  NOTEBOOK_ARGS="--hub-prefix=$JPY_HUB_PREFIX $NOTEBOOK_ARGS"
+    NOTEBOOK_ARGS="--hub-prefix=$JPY_HUB_PREFIX $NOTEBOOK_ARGS"
 fi
 if [ ! -z "$JPY_HUB_API_URL" ]; then
-  NOTEBOOK_ARGS="--hub-api-url=$JPY_HUB_API_URL $NOTEBOOK_ARGS"
+    NOTEBOOK_ARGS="--hub-api-url=$JPY_HUB_API_URL $NOTEBOOK_ARGS"
 fi
-if [ ! -z "$JUPYTER_ENABLE_LAB" ]; then
-  NOTEBOOK_BIN="jupyter labhub"
-else
-  NOTEBOOK_BIN="jupyterhub-singleuser"
-fi
+NOTEBOOK_BIN="jupyterhub-singleuser"
 
 . /usr/local/bin/start.sh $NOTEBOOK_BIN $NOTEBOOK_ARGS "$@"
