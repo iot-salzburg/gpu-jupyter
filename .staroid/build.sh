614135933b3c990253e593e1ca69c8a04708c3d9
Lee moon soo <leemoonsoo@gmail.com>
2020-08-03 15:36:59 -0700

    add starod integration

---
+++ .staroid/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Custom builder script for Skaffold
+# https://skaffold.dev/docs/pipeline-stages/builders/custom/
+#
+
+# generate Dockerfile
+./generate-Dockerfile.sh -s --no-datascience-notebook --no-useful-packages
+cd .build
+
+# print Dockerfile
+cat Dockerfile
+
+# build
+docker build -f Dockerfile -t $IMAGE .
+
+if $PUSH_IMAGE; then
+  docker push $IMAGE
+fi
