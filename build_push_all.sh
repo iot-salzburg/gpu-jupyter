488ebea0c27b1c350a4c372dfe912738b446db4c
Christoph Schranz <christoph.schranz@salzburgresearch.at>
2021-02-05 12:26:47 +0100

    no zip, also tag python-only

--- build_push_all.sh
+++ build_push_all.sh
@@ -18,8 +18,7 @@ docker tag $IMG_ID cschranz/gpu-jupyter:$TAGNAME
 docker rm -f gpu-jupyter_1
 docker run --gpus all -d -it -p 8848:8888 -v $(pwd)/data:/home/jovyan/work -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes --user root --restart always --name gpu-jupyter_1 cschranz/gpu-jupyter:$TAGNAME
 
-docker push cschranz/gpu-jupyter:$TAGNAME &&
-docker save cschranz/gpu-jupyter:$TAGNAME | gzip > ../gpu-jupyter_tag-$TAGNAME.tar.gz
+docker push cschranz/gpu-jupyter:$TAGNAME
 
 
 ###################### build and push slim image ##########################
@@ -33,22 +32,18 @@ export IMG_ID=$(docker image ls | grep ${TAGNAME}_slim  | head -1 | awk '{print 
 echo "push image with ID $IMG_ID and Tag ${TAGNAME}_slim."
 
 docker tag $IMG_ID cschranz/gpu-jupyter:${TAGNAME}_slim
-docker push cschranz/gpu-jupyter:${TAGNAME}_slim &&
-docker save cschranz/gpu-jupyter:${TAGNAME}_slim | gzip > ../gpu-jupyter_tag-${TAGNAME}_slim.tar.gz
-
+docker push cschranz/gpu-jupyter:${TAGNAME}_slim
 
 
 ###################### build and push python-only image ##########################
 echo
 echo
-echo "build and push slim image with tag ${TAGNAME}_slim."
-bash generate-Dockerfile.sh --slim
-docker build -t cschranz/gpu-jupyter:${TAGNAME}_slim  .build/
+echo "build and push slim image with tag ${TAGNAME}_python-only."
+bash generate-Dockerfile.sh --python-only
+docker build -t cschranz/gpu-jupyter:${TAGNAME}_python-only  .build/
 
-export IMG_ID=$(docker image ls | grep ${TAGNAME}_slim  | head -1 | awk '{print $3}')
-echo "push image with ID $IMG_ID and Tag ${TAGNAME}_slim."
+export IMG_ID=$(docker image ls | grep ${TAGNAME}_python-only  | head -1 | awk '{print $3}')
+echo "push image with ID $IMG_ID and Tag ${TAGNAME}_python-only."
 
-docker tag $IMG_ID cschranz/gpu-jupyter:${TAGNAME}_slim
-docker push cschranz/gpu-jupyter:${TAGNAME}_slim &&
-docker save cschranz/gpu-jupyter:${TAGNAME}_slim | gzip > ../gpu-jupyter_tag-${TAGNAME}_slim.tar.gz
-
+docker tag $IMG_ID cschranz/gpu-jupyter:${TAGNAME}_python-only
+docker push cschranz/gpu-jupyter:${TAGNAME}_python-only
