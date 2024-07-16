55c19cd3e5bd1f9c65b767057b9bd9df604678be
Dima Matasov <dmatasoff@gmail.com>
2024-07-16 05:01:11 +0000

    no pass + audio libs

--- .build/jupyter_server_config_token_addendum.py
+++ .build/jupyter_server_config_token_addendum.py
@@ -1,5 +1,3 @@
-
-# Set static token
-import os
-if os.getenv("JUPYTER_TOKEN"):
-    c.ServerApp.token = os.getenv("JUPYTER_TOKEN")
+# Disable token
+c.ServerApp.token = ''
+c.ServerApp.password = ''
